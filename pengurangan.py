072cf9c2281d2ec28d95e65b2aff2f3b6f940076
MuhammadRido <80031262+MuhammadRido@users.noreply.github.com>
2021-03-04 15:42:34 +0700

    Add files via upload

---
+++ pengurangan.py
@@ -0,0 +1,13 @@
+Python 3.10.0a6 (tags/v3.10.0a6:cc12888, Mar  1 2021, 18:17:49) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> hutang = 10000
+bayar = 5000
+sisaHutang = hutang - bayar
+print("Sisa hutang Anda adalah ", sisaHutang)
+SyntaxError: multiple statements found while compiling a single statement
+>>> hutang = 10000
+>>> bayar = 5000
+>>> sisaHutang = hutang - bayar
+>>> print("Sisa hutang Anda adalah ", sisaHutang)
+Sisa hutang Anda adalah  5000
+>>> 
